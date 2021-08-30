ed74f2c01449175098eef9d93b8fb5e5647ceb02
Guilherme <hfdsguilherme@gmail.com>
2021-08-30 18:20:56 -0300

    chore: prefetch user

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,13 @@ import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../contexts/SidebatDrawerContext'
 import { makeServer } from '../services/mirage'
 import { QueryClient, QueryClientProvider } from "react-query"
+import { queryClient } from '../services/queryClient'
 
 if(process.env.NODE_ENV === "development") {
   makeServer()
 }
 
-const queryClient = new QueryClient()
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,14 @@
-import { Box, Flex, Button, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
+import { Box, Flex, Button, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
-import Link from "next/link"
+import NextLink from "next/link"
 import { useUsers } from "../../services/hooks/useUsers";
 import { useState } from "react";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
 
 export default function UserList() {
 	const [page, setPage] = useState(1)
@@ -16,6 +18,14 @@ export default function UserList() {
 		base: false,
 		lg: true
 	})
+
+	async function handlePrefetchUser(userId: number) {
+		await queryClient.prefetchQuery(['user', userId], async () => {
+			const response = await api.get(`users/${userId}`)
+
+			return response.data
+		})
+	}
 
 	return (
 		<Box>
@@ -30,7 +40,7 @@ export default function UserList() {
 							Usuários
 							{ !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}	
 						</Heading>
-						<Link href="/users/create" passHref>
+						<NextLink href="/users/create" passHref>
 							<Button
 								as="a"
 								size="sm"
@@ -40,7 +50,7 @@ export default function UserList() {
 							>
 								Criar novo
 							</Button>
-						</Link>
+						</NextLink>
 					</Flex>
 
 					{ isLoading ? (
@@ -73,7 +83,9 @@ export default function UserList() {
 												</Td>
 												<Td>
 													<Box>
-														<Text fontWeight="bold">{user.name}</Text>
+														<Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+															<Text fontWeight="bold">{user.name}</Text>
+														</Link>
 														<Text fontSize="sm" color="gray.300">{user.email}</Text>
 													</Box>
 												</Td>
