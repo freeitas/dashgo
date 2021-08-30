8302d937c304578976c95e9c759468bfc7597ce1
Guilherme <hfdsguilherme@gmail.com>
2021-08-30 17:51:51 -0300

    chore: change page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,9 +6,11 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link"
 import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
 
 export default function UserList() {
-	const { data, isLoading, isFetching, error } = useUsers()
+	const [page, setPage] = useState(1)
+	const { data, isLoading, isFetching, error } = useUsers(page)
 
 	const isWideVersion = useBreakpointValue({
 		base: false,
@@ -63,7 +65,7 @@ export default function UserList() {
 									</Tr>
 								</Thead>
 								<Tbody>
-									{data.map(user => {
+									{data.users.map(user => {
 										return (
 											<Tr key={user.id}>
 												<Td px={["4", "4", "6"]}>
@@ -93,9 +95,9 @@ export default function UserList() {
 								</Tbody>
 							</Table>
 							<Pagination
-								totalCountofRegisters={200}
-								currentPage={5}
-								onPageChange={() => {}}
+								totalCountofRegisters={data.totalCount}
+								currentPage={page}
+								onPageChange={setPage}
 							/>
 						</>
 					)}
